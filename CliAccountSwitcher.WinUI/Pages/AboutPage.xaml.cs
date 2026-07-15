5204e74d414c16f05a366fa4172ff8730a301691
Howon Lee <kck4156@kagamine-rin.com>
2026-07-15 12:26:42 +0900

    chore: update dependency versions in About page

--- CliAccountSwitcher.WinUI/Pages/AboutPage.xaml.cs
+++ CliAccountSwitcher.WinUI/Pages/AboutPage.xaml.cs
@@ -64,13 +64,13 @@ public sealed partial class AboutPage : Page
         [
             new("CommunityToolkit.Mvvm", "8.4.2", "MIT", "Microsoft", "https://github.com/CommunityToolkit/dotnet"),
             new("CommunityToolkit.WinUI.Converters", "8.2.251219", "MIT", "Microsoft.Toolkit", "https://github.com/CommunityToolkit/Windows"),
-            new("Deskband11Lib.WinUI", "1.1.0", "MIT", "airtaxi", "https://github.com/airtaxi/Deskband11Lib"),
+            new("Deskband11Lib.WinUI", "1.3.6", "MIT", "airtaxi", "https://github.com/airtaxi/Deskband11Lib"),
             new("DevWinUI.Controls", "9.9.4", "MIT", "Mahdi Hosseini", "https://github.com/ghost1372/DevWinUI"),
             new("H.NotifyIcon.WinUI", "2.4.1", "MIT", "havendv", "https://github.com/HavenDV/H.NotifyIcon"),
-            new("Microsoft.Extensions.DependencyInjection", "10.0.9", "MIT", "Microsoft", "https://github.com/dotnet/runtime"),
-            new("Microsoft.Windows.SDK.BuildTools", "10.0.28000.1839", windowsSoftwareDevelopmentKitLicenseText, "Microsoft", "https://aka.ms/WinSDKProjectURL"),
+            new("Microsoft.Extensions.DependencyInjection", "10.0.10", "MIT", "Microsoft", "https://github.com/dotnet/runtime"),
+            new("Microsoft.Windows.SDK.BuildTools", "10.0.28000.2270", windowsSoftwareDevelopmentKitLicenseText, "Microsoft", "https://aka.ms/WinSDKProjectURL"),
             new("Microsoft.WindowsAppSDK", "2.2.0", "MIT", "Microsoft", "https://github.com/microsoft/windowsappsdk"),
-            new("WinUIEx", "2.9.1", "MIT", "Morten Nielsen", "https://dotmorten.github.io/WinUIEx")
+            new("WinUIEx", "2.9.2", "MIT", "Morten Nielsen", "https://dotmorten.github.io/WinUIEx")
         ];
     }
 
